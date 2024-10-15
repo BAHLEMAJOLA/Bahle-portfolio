d19c2c1ef813933031ef4114df825ed6273bfd91
Bahle Ntshayisa <ntshayisabuhle@gmail.com>
2024-10-15 22:36:23 +0200

    made changes on html and css

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-const menu = document.getElementById('menu');
-const links = document.querySelector('ul.links');
-const contactBtn = document.querySelector('.btn');
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.getElementById('menu');
+    const links = document.querySelector('ul.links');
+    const contactBtn = document.querySelector('.btn');
 
-menu.addEventListener('click', () => {
-    links.classList.toggle('active');
-    contactBtn.classList.toggle('active');
-});
+    menu.addEventListener('click', () => {
+        links.classList.toggle('active');
+        contactBtn.classList.toggle('active');
+    });
 
-// Optional: Close the menu when a link is clicked
-const navLinks = document.querySelectorAll('ul.links a');
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        links.classList.remove('active');
-        contactBtn.classList.remove('active');
+    const navLinks = document.querySelectorAll('ul.links a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            links.classList.remove('active');
+            contactBtn.classList.remove('active');
+        });
     });
 });
+
